b8a988c428f17648b252d6db4624dfaa25df6a8a
David Shen <me@pantherman594.com>
2019-04-13 22:15:23 -0400

    Fix OTP files

--- backend/update.sh
+++ backend/update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+cd otp
+
 rm MBTA_GTFS.zip
 wget https://cdn.mbta.com/MBTA_GTFS.zip
 
 rm mass.pbf
 wget http://download.geofabrik.de/north-america/us/massachusetts-latest.osm.pbf
-osmconvert massachusetts-latest.osm.pbf -b=41.76,-72.05,42.81,-70.45 --complete-ways -o=mass.pbf
+osmconvert ../massachusetts-latest.osm.pbf -b=-72.05,41.76,-70.45,42.81 --complete-ways -o=mass.pbf
 rm massachusetts-latest.osm.pbf
